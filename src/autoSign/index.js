bf1a15fbda6a306ca39e4079fe0d6f04a431914a
李连政 <tony.li@aiyuangong.com>
2018-08-22 19:54:03 +0800

    自动签到

--- src/autoSign/index.js
+++ src/autoSign/index.js
@@ -1,11 +1,11 @@
 const got = require("got");
 const CronJob = require("cron").CronJob;
 const emailUtil = require('../util/emailUtil')
-
+const token = "0r1p0u1huua04cqs2e9bm0egv6";
+const token_xt = "4uea6qv3e5khb2ag22lq6hnmmo";
 const host = "https://www.tokenworld.pro";
 const signUrl = "/japi/signIn/signing/14";
 const signStatUrl = "/japi/signIn/getStat/13";
-
 /**
  * 
  * @param {string} label 标签 optional
@@ -19,7 +19,7 @@ function sign(label,sid) {
     }
   }).then(response => {
     console.log(response.body);
-    let html=label+Date.now();
+    let html=label+"签到结果通知";
     if (response.body && response.body.status == 200) {
       emailUtil.sendMail("383455526@qq.com","自动签到测试成功",html)
     }else {
@@ -29,7 +29,7 @@ function sign(label,sid) {
 }
 
 const signJob = new CronJob({
-  cronTime: "00 30 10 * * *",
+  cronTime: "00 30 11 * * *",
   onTick: () => {
     sign("13143683782",token);
     sign("13242878816",token_xt);
--- src/crawler/crawler-union100.js
+++ src/crawler/crawler-union100.js
@@ -1,2 +1,1 @@
 const rquest = require('request')
-
